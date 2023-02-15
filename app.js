b26dfa596fc4443a800c1499b41c577c2de45caf
RonaldoDairo <programaciondairo@gmail.com>
2023-02-15 00:02:37 +0000

    Generador de Nombres de Dominios

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
+
+pronoun.filter((primero)=>{
+    adj.filter((segundo) =>{
+       noun.filter((tercero)=>{
+        console.log(primero+''+segundo+''+tercero +'.com')
+       });
+    });
+});
+
