cc86ffe7f410f32c413aa1c8ff77ccce4d78bb94
RonaldoDairo <programaciondairo@gmail.com>
2023-02-20 21:34:22 +0000

    cambiando de un filter a forEach

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
 
-pronoun.filter((primero)=>{
-    adj.filter((segundo) =>{
-       noun.filter((tercero)=>{
+pronoun.forEach((primero)=>{
+    adj.forEach((segundo) =>{
+       noun.forEach((tercero)=>{
         console.log(primero+''+segundo+''+tercero +'.com')
        });
     });
